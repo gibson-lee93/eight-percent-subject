25de1a8c5e269ed0bf95e367932fec80094f586e
lhj0621 <lhj0621@gmail.com>
2021-11-11 23:05:37 +0900

    feat: 로그인
    - 인증 관련 auth 모듈 추가
    - jwt sign을 통한 accessToken 발급
    
    resolves: #3

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { SigninDto } from './dto/signin.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -10,5 +11,10 @@ export class UsersController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.usersService.createUser(createUserDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body() signinDto: SigninDto): Promise<{ accessToken }> {
+    return this.usersService.signIn(signinDto);
   }
 }
