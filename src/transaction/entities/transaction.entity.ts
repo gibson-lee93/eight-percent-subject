404d8e820d359b784051ac14a7ba1421443ca7d5
doogler777 <doogler777@gmail.com>
2021-11-11 20:24:10 +0900

    feat:transaction기본설정 - entity 생성, createDto생성 - resolves #20

---
+++ src/transaction/entities/transaction.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  trans_type: string;
+
+  @Column()
+  amount: bigint;
+
+  @Column({ default: '' })
+  comments: string;
+
+  @Column({ default: 0 })
+  balance: bigint;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  // 나중에 account테이블 생성되면 사용
+  // @ManyToOne((_type) => Account, (account) => account.transactions, {
+  //   eager: false,
+  //   onDelete: 'CASCADE',
+  // })
+  // account: Account;
+}
