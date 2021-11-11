d60bef856bfd98c9334ff444b5c388cd7e8dd75d
yhshin0 <asyh1047@naver.com>
2021-11-11 22:56:19 +0900

    fix: update-account dto
    - money 값 제한 제거
    resolves: #5

--- src/accounts/dto/update-account.dto.ts
+++ src/accounts/dto/update-account.dto.ts
@@ -1,9 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNumber, Min } from 'class-validator';
+import { IsNumber } from 'class-validator';
 import { CreateAccountDto } from './create-account.dto';
 
 export class UpdateAccountDto extends PartialType(CreateAccountDto) {
   @IsNumber()
-  @Min(0)
   money: number;
 }
