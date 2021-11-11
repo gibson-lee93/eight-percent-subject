4d86df71e12c688262b4a7fde858dd7514283dc2
lhj0621 <lhj0621@gmail.com>
2021-11-11 20:49:51 +0900

    feat : 유저 기본 세팅 & 회원 가입
    - entitiy 정의
    - 중복 아이디 체크
    - 기본 세팅
    
    resolves: #2

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
+import { CoreEntity } from 'src/core/entities/core.entity';
+
+@Entity()
+export class User extends CoreEntity {
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'datetime', nullable: true })
+  loginedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
