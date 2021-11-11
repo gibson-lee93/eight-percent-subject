7b3a361333a90723f04345ed3416efabfe505397
BangleCoding <effectivecodeyr@gmail.com>
2021-11-11 23:42:22 +0900

    feat: 거래내역 전체 조회 진행중 - 거래내역 조건문 작성중, interface 작성, pagination 기능 완성 resolves #14

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionRepository } from 'typeorm';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TransactionRepository])],
   controllers: [TransactionController],
-  providers: [TransactionService]
+  providers: [TransactionService],
 })
 export class TransactionModule {}
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { PagingOptions } from './transaction.interface';
+import { TransactionRepository } from './transaction.repository';
 @Injectable()
-export class TransactionService {}
+export class TransactionService {
+  constructor(
+    @InjectRepository(TransactionRepository)
+    private readonly transactionRepository: TransactionRepository,
+  ) {}
+
+  async getAllTransactions(query: PagingOptions): Promise<Transaction[]> {
+    const result = this.transactionRepository.getAllTransactions(query);
+
+    return result;
+  }
+}
