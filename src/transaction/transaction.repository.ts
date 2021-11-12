7da147b6332cd3e684f54f97f20a3bbcdcc1768b
Chinsan Chung <chinsanchung@gmail.com>
2021-11-12 20:09:00 +0900

    fix: 입금 출금 필터링 수정
    - 입금, 출금뿐만 아니라 입출금 모두 출력하도록 수정했습니다.
    - 요청할 때 쿼리에 trans_type 을 제거하면 입출금 모두를 출력합니다.
    
    resolves: #14

--- src/transaction/transaction.repository.ts
+++ src/transaction/transaction.repository.ts
@@ -47,6 +47,17 @@ export class TransactionRepository extends Repository<Transaction> {
       startDate,
       endDate,
     );
+    // * 입금, 출금 필터링. 1. 입금, 2. 출금, 3. 입출금 으로 구분합니다.
+    let transTypeQuery: any = [
+      'transaction.trans_type = :trans_type',
+      { trans_type },
+    ];
+    if (!trans_type) {
+      transTypeQuery = [
+        'transaction.trans_type IN (:...trans_type)',
+        { trans_type: ['in', 'out'] },
+      ];
+    }
     const transaction = await this.createQueryBuilder('transaction')
       .leftJoinAndSelect('transaction.account', 'account')
       .where('account.id = :account_id', { account_id })
@@ -56,7 +67,7 @@ export class TransactionRepository extends Repository<Transaction> {
       .andWhere('transaction.createdAt <= :endDate', {
         endDate: endDateString,
       })
-      .andWhere('transaction.trans_type = :trans_type', { trans_type }) // * 입금, 출금 필터링
+      .andWhere(transTypeQuery[0], transTypeQuery[1])
       .limit(limit) // * Pagination 기능
       .offset(offset) // * Pagination 기능
       .select([
