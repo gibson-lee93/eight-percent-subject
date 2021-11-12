5603aabc3f3ec4cb437daa9bf1d2c5b0ff502948
Chinsan Chung <chinsanchung@gmail.com>
2021-11-12 14:39:57 +0900

    feat: 거래내역 조회 목록 진행 - 날짜로 필터링, 계좌 테이블과 조인 수정
    - 날짜 필터링 기능 추가
    - moment-timezone을 설치해서 시작일과 마지막일의 기본값을 설정("3개월 전 ~ 오늘"으로 설정)
    - transaction 엔티티에 eager 설정을 true 로 수정해서 거래내역 목록에 계좌의 아이디가 나오도록 설정
    
    resolves: #14, #17

--- src/transaction/transaction.interface.ts
+++ src/transaction/transaction.interface.ts
@@ -1,9 +1,9 @@
 export interface ListQueryOptions {
-  acc_num?: string;
+  account_id?: string;
   page: string;
   trans_type: string;
-  startDate: string;
-  endDate: string;
+  startDate?: string;
+  endDate?: string;
 }
 
 export interface PagingOptions extends ListQueryOptions {
