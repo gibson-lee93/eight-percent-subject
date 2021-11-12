f04b88c1f157944850340590b518130346d39a32
doogler777 <doogler777@gmail.com>
2021-11-13 00:28:06 +0900

    test:입금,출금 컨트롤러 테스트 추가

--- src/transaction/transaction.controller.spec.ts
+++ src/transaction/transaction.controller.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'src/users/entities/user.entity';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { TransactionController } from './transaction.controller';
+import { TransactionService } from './transaction.service';
+
+jest.mock('./transaction.service');
 
 describe('TransactionController', () => {
   let controller: TransactionController;
+  let service: TransactionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionController],
+      providers: [TransactionService],
     }).compile();
 
     controller = module.get<TransactionController>(TransactionController);
+    service = module.get<TransactionService>(TransactionService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  const user: User = {
+    id: 1,
+    user_id: '1',
+    password: '123',
+    hashPassword: undefined,
+    loginedAt: new Date(),
+    accounts: null,
+    deletedAt: null,
+    updatedAt: new Date(),
+    createdAt: new Date(),
+  };
+  const createTransactionDto: CreateTransactionDto = {
+    acc_num: 'testNum',
+    amount: 1000,
+    comments: 'testComments',
+  };
+  describe('deposit', () => {
+    it('입금', async () => {
+      jest.spyOn(service, 'deposit').mockResolvedValue({ message: 'ok' });
+      const result = await controller.deposit(user, createTransactionDto);
+      expect(result).toEqual({ message: 'ok' });
+    });
+  });
+  describe('withdraw', () => {
+    it('출금', async () => {
+      jest.spyOn(service, 'withdraw').mockResolvedValue({ message: 'ok' });
+      const result = await controller.withdraw(user, createTransactionDto);
+      expect(result).toEqual({ message: 'ok' });
+    });
   });
 });
