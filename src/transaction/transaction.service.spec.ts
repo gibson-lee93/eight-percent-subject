979d0f8a05068e685dcc3faba0b354f000e0fe45
doogler777 <doogler777@gmail.com>
2021-11-12 20:58:03 +0900

    test:deposit 테스트 임시저장

--- src/transaction/transaction.service.spec.ts
+++ src/transaction/transaction.service.spec.ts
@@ -1,18 +1,140 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AccountsRepository } from '../accounts/accounts.repository';
+import { Account } from '../accounts/entities/account.entity';
+import {
+  Connection,
+  getCustomRepository,
+  QueryRunner,
+  Repository,
+} from 'typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { TransactionRepository } from './transaction.repository';
 import { TransactionService } from './transaction.service';
+import { User } from 'src/users/entities/user.entity';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+
+const mockTransactionRepository = () => ({
+  save: jest.fn(),
+  create: jest.fn(),
+});
+
+const mockAccountsRepository = () => ({
+  findOneByAccountNumber: jest.fn(),
+  updateAccount: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('TransactionService', () => {
   let service: TransactionService;
+  let connection: Connection;
+  let accountsRepository: MockRepository<AccountsRepository>;
+  let transactionRepository: MockRepository<TransactionRepository>;
+
+  const qr = {
+    manager: {},
+  } as QueryRunner;
+
+  class ConnectionMock {
+    createQueryRunner(mode?: 'master' | 'slave'): QueryRunner {
+      return qr;
+    }
+  }
 
   beforeEach(async () => {
+    Object.assign(qr.manager, {
+      save: jest.fn(),
+    });
+    qr.connect = jest.fn();
+    qr.release = jest.fn();
+    qr.startTransaction = jest.fn();
+    qr.commitTransaction = jest.fn();
+    qr.rollbackTransaction = jest.fn();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionService],
+      providers: [
+        TransactionService,
+        {
+          provide: getRepositoryToken(Account),
+          useValue: mockAccountsRepository,
+        },
+        {
+          provide: getRepositoryToken(Transaction),
+          useValue: mockTransactionRepository,
+        },
+        { provide: Connection, useClass: ConnectionMock },
+      ],
     }).compile();
 
     service = module.get<TransactionService>(TransactionService);
+    connection = module.get<Connection>(Connection);
+    transactionRepository = module.get<MockRepository<Transaction>>(
+      getRepositoryToken(Transaction),
+    );
+    accountsRepository = module.get<MockRepository<Account>>(
+      getRepositoryToken(Transaction),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(connection).toBeDefined();
+    expect(transactionRepository).toBeDefined();
+    expect(accountsRepository).toBeDefined();
+  });
+
+  const user: User = {
+    id: 1,
+    user_id: '1',
+    password: '123',
+    hashPassword: undefined,
+    loginedAt: new Date(),
+    accounts: null,
+    deletedAt: null,
+    updatedAt: new Date(),
+    createdAt: new Date(),
+  };
+  const createTransactionDto: CreateTransactionDto = {
+    acc_num: 'testNum',
+    amount: 1000,
+    comments: 'testComments',
+  };
+
+  describe('deposit', () => {
+    let queryRunner: QueryRunner;
+
+    beforeEach(() => {
+      queryRunner = connection.createQueryRunner();
+      jest.spyOn(queryRunner, 'connect').mockResolvedValueOnce(undefined);
+      jest
+        .spyOn(queryRunner, 'startTransaction')
+        .mockResolvedValueOnce(undefined);
+    });
+
+    afterEach(() => {
+      jest.spyOn(queryRunner, 'release').mockResolvedValue();
+    });
+    const accout: Account = {
+      id: 1,
+      acc_num: 'test',
+      money: 100,
+      user: null,
+      deletedAt: null,
+      updatedAt: new Date(),
+      createdAt: new Date(),
+      transactions: null,
+    };
+
+    it('계좌입금에 성공', async () => {
+      const userRepo = {
+        findOneByAccountNumber: jest.fn().mockResolvedValueOnce('fake user'),
+      };
+      jest
+        .spyOn(queryRunner.manager, 'getCustomRepository')
+        .mockResolvedValue();
+      const result = await service.deposit(user, createTransactionDto);
+      expect(result).toEqual({ message: 'ok' });
+    });
   });
 });
