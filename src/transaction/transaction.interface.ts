f3c6e6759e6342849283cb3f7737fa42e81d7a62
Chinsan Chung <chinsanchung@gmail.com>
2021-11-12 17:04:31 +0900

    fix: 리스트 메소드의 쿼리 인터페이스 수정
    - trans_type 을 string 에서 'in' | 'out' 으로 수정
    
    resolves: #14

--- src/transaction/transaction.interface.ts
+++ src/transaction/transaction.interface.ts
@@ -3,7 +3,7 @@ import { User } from '../users/entities/user.entity';
 export interface ListQueryOptions {
   account_id?: string;
   page: string;
-  trans_type: string;
+  trans_type: 'in' | 'out';
   startDate?: string;
   endDate?: string;
 }
