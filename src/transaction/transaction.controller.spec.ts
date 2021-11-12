1535fbeeb3c18180a281f26e8273b477ed04183a
Chinsan Chung <chinsanchung@gmail.com>
2021-11-13 02:02:07 +0900

    test: 목록 조회, 목록 상세 보기 테스트 코드 작성
    - 입금 목록과 출금 목록을 테스트하는 코드를 작성했습니다.
    - 특정 목록의 상세 보기를 테스트하는 코드를 작성했습니다.
    
    resolves: #16
    Co-Authored-By: BangleCoder <47234375+BangleCoding@users.noreply.github.com>

--- src/transaction/transaction.controller.spec.ts
+++ src/transaction/transaction.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { User } from 'src/users/entities/user.entity';
+import { Account } from '../accounts/entities/account.entity';
+import { User } from '../users/entities/user.entity';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { Transaction } from './entities/transaction.entity';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 
@@ -41,6 +43,18 @@ describe('TransactionController', () => {
     amount: 1000,
     comments: 'testComments',
   };
+
+  const mockAccount = new Account();
+  mockAccount.id = 1;
+  mockAccount.acc_num = '123-456-789';
+  mockAccount.money = 100000;
+  const mockTransaction = new Transaction();
+  mockTransaction.id = 1;
+  mockTransaction.amount = 1000;
+  mockTransaction.comments = 'new transaction';
+  mockTransaction.balance = 500;
+  mockTransaction.createdAt = new Date();
+
   describe('deposit', () => {
     it('입금', async () => {
       jest.spyOn(service, 'deposit').mockResolvedValue({ message: 'ok' });
@@ -53,6 +67,82 @@ describe('TransactionController', () => {
       jest.spyOn(service, 'withdraw').mockResolvedValue({ message: 'ok' });
       const result = await controller.withdraw(user, createTransactionDto);
       expect(result).toEqual({ message: 'ok' });
+    });
+  });
+
+  describe('getMany', () => {
+    const mockListQuery = {
+      account_id: '1',
+      page: '1',
+      startDate: '2021-11-01',
+      endDate: '2021-11-13',
+    };
+
+    it('입금 목록을 출력합니다.', async () => {
+      mockTransaction.trans_type = 'in';
+      mockTransaction.account = mockAccount;
+      Object.assign(mockListQuery, { trans_type: 'in' });
+      jest
+        .spyOn(service, 'getAllTransactions')
+        .mockResolvedValue([mockTransaction]);
+
+      const expectedResult = await controller.getAllTransactions(
+        mockListQuery,
+        user,
+      );
+      expect(expectedResult).toMatchObject([mockTransaction]);
+    });
+    it('출금 목록을 출력합니다.', async () => {
+      mockTransaction.trans_type = 'out';
+      mockTransaction.account = mockAccount;
+      Object.assign(mockListQuery, { trans_type: 'out' });
+      jest
+        .spyOn(service, 'getAllTransactions')
+        .mockResolvedValue([mockTransaction]);
+
+      const expectedResult = await controller.getAllTransactions(
+        mockListQuery,
+        user,
+      );
+      expect(expectedResult).toMatchObject([mockTransaction]);
+    });
+    it('입금, 출금 목록을 출력합니다.', async () => {
+      mockTransaction.trans_type = 'in';
+      mockTransaction.account = mockAccount;
+      const mockWithdrawTransaction = new Transaction();
+      mockWithdrawTransaction.id = 2;
+      mockWithdrawTransaction.amount = 1000;
+      mockWithdrawTransaction.comments = 'new transaction';
+      mockWithdrawTransaction.balance = 500;
+      mockWithdrawTransaction.createdAt = new Date();
+      mockWithdrawTransaction.account = mockAccount;
+
+      Object.assign(mockListQuery);
+      jest
+        .spyOn(service, 'getAllTransactions')
+        .mockResolvedValue([mockTransaction, mockWithdrawTransaction]);
+
+      const expectedResult = await controller.getAllTransactions(
+        mockListQuery,
+        user,
+      );
+      expect(expectedResult).toMatchObject([
+        mockTransaction,
+        mockWithdrawTransaction,
+      ]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('거래내역을 조회한다.', async () => {
+      mockTransaction.trans_type = 'in';
+      mockTransaction.account = mockAccount;
+      const id = '1';
+      jest
+        .spyOn(service, 'getOneTransaction')
+        .mockResolvedValue(mockTransaction);
+      const expectTransaction = await controller.findOneTransaction(id);
+      expect(expectTransaction).toMatchObject(mockTransaction);
     });
   });
 });
